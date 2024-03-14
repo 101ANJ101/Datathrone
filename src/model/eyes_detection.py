29323bff29e7e2e970c6a1f09b499888fa5b45dc
Pranjal Mantri <pranjalmantri@gmail.com>
2024-03-14 15:59:49 +0530

    build eye_detection model

---
+++ src/model/eyes_detection.py
@@ -0,0 +1,85 @@
+import cv2
+import dlib
+import numpy as np
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("src/model/shape_predictor_68_face_landmarks.dat")
+
+def detect_eyes(frame_gray, frame, frame_number):
+    print(f"Frame Number", frame_number)
+    faces = detector(frame_gray)
+
+    status = "Unknown"
+
+    for face in faces:
+        landmarks = predictor(frame_gray, face)
+
+        left_eye_indices = list(range(36, 42))
+        right_eye_indices = list(range(42, 48))
+
+        left_eye_points = [(landmarks.part(idx).x, landmarks.part(idx).y) for idx in left_eye_indices]
+        right_eye_points = [(landmarks.part(idx).x, landmarks.part(idx).y) for idx in right_eye_indices]
+
+        left_eye_points = np.array(left_eye_points, np.int32)
+        right_eye_points = np.array(right_eye_points, np.int32)
+
+        cv2.polylines(frame, [left_eye_points], True, (0, 0, 255), 1)
+        cv2.polylines(frame, [right_eye_points], True, (0, 0, 255), 1)
+
+        left_ear = eye_aspect_ratio(left_eye_points)
+        right_ear = eye_aspect_ratio(right_eye_points)
+
+        ear = (left_ear + right_ear) / 2
+
+        closed_ear_thresh = 0.6
+
+        # Classify the eyes as open or closed based on the EAR threshold
+        if ear < closed_ear_thresh:
+            status = "Closed"
+            cv2.putText(frame, 'Closed', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        else:
+            status = "Open"
+            cv2.putText(frame, 'Open', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        
+    return frame, status
+
+def eye_aspect_ratio(eye_points):
+    vertical_dist1 = abs(((eye_points[1][0] - eye_points[5][0]) * 2 + (eye_points[1][1] - eye_points[5][1]) * 2)) ** 0.5
+    vertical_dist2 = abs(((eye_points[2][0] - eye_points[4][0]) * 2 + (eye_points[2][1] - eye_points[4][1]) * 2)) ** 0.5
+
+    horizontal_dist = abs(((eye_points[0][0] - eye_points[3][0]) * 2 + (eye_points[0][1] - eye_points[3][1]) * 2)) ** 0.5
+
+    ear = (vertical_dist1 + vertical_dist2) / (2 * horizontal_dist)
+
+    return ear
+
+cap = cv2.VideoCapture("src/model/I ruined everything..mp4")
+frame_count = 0
+skip_ratio = 10
+frame_number = 0
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+    
+    frame_count += 1
+    frame_number += 1
+
+    if frame_count % skip_ratio != 0:
+        continue 
+
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame, status = detect_eyes(frame_gray, frame, frame_number)
+
+    if status == "Open":
+        cv2.imwrite(str(frame_number) + ".jpg", frame)
+
+
+    if (cv2.waitKey(30) == 27):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
